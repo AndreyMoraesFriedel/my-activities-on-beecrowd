7e1107cb1475322d29043b9cba445ceac0f529c4
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2025-09-12 18:22:44 -0300

    Area of a Circle in C++: Beecrowd 1002

---
+++ Beecrowd 1002/AreaofaCircle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    double raio;
+    cin >> raio;
+    double pi = 3.14159;
+    double area = pi * pow(raio, 2);
+    cout << "A=" << fixed << setprecision(4) << area << endl;
+    return 0;
+}
