88c589231c97489e81720754536599272d37772c
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2025-09-12 19:11:41 -0300

    SOMA in C: Beecrowd 1003

---
+++ Beecrowd 1003/Soma.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int number1, number2;
+    scanf("%d", &number1);
+    scanf("%d", &number2);
+    int soma = number1 + number2;
+    printf("SOMA = %d\n", soma);
+    return 0;
+}
