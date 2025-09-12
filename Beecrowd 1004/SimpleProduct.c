57cd2be45d6b86f5e2e7bfc295fe5602224b7976
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2025-09-12 19:14:13 -0300

    Simple Product in C: Beecrowd 1004

---
+++ Beecrowd 1004/SimpleProduct.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int number1, number2;
+    scanf("%d", &number1);
+    scanf("%d", &number2);
+    int prod = number1 * number2;
+    printf("PROD = %d\n", prod);
+    return 0;
+}
