eb4305710be72f8fc3d794c7a86efc997e0a1402
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2025-09-12 18:06:59 -0300

    ExtremeBasic in C: Beecrowd 1001

---
+++ Beecrowd 1001/ExtremeBasic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int number1, number2;
+    scanf("%d", &number1);
+    scanf("%d", &number2);
+    int x = number1 + number2;
+    printf("X = %d\n", x);
+    return 0;
+}
